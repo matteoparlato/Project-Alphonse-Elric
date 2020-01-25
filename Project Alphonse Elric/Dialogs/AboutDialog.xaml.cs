a63701178afdc2cb08453998a973f1503245dacc
Matteo Parlato <matteo.parlato@outlook.it>
2020-01-25 11:15:56 +0100

    Privacy Policy Update

--- Project Alphonse Elric/Dialogs/AboutDialog.xaml.cs
+++ Project Alphonse Elric/Dialogs/AboutDialog.xaml.cs
@@ -3,12 +3,12 @@
 namespace Project_Alphonse_Elric.Dialogs
 {
     /// <summary>
-    /// About class
+    /// AboutDialog class
     /// </summary>
     public sealed partial class AboutDialog : ContentDialog
     {
         /// <summary>
-        /// Parameterless constructor of About class.
+        /// Parameterless constructor of AboutDialog class.
         /// </summary>
         public AboutDialog()
         {
